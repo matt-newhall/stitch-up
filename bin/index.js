ed92607e9f266ede87eae41e75ef0160f46280d1
matt-newhall <matt@newhall.dev>
2023-12-27 23:49:56 +0000

    feat: add package and repo settings

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,31 @@
 #! /usr/bin/env node
-console.log("Hello, World!");
+
+import { exec } from 'child_process';
+import inquirer from 'inquirer';
+
+const repoName = process.argv[2]
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'repoSetting',
+      message: 'How would you like to configure repo?',
+      choices: ["Frontend Only", "Fullstack"],
+    },
+  ])
+  .then(answer => {
+    const isFullstack = answer.repoSetting === 'Fullstack'
+    const command = `bin/run-create.sh ${repoName} ${isFullstack}`
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Error: ${stderr}`);
+        return;
+      }
+      console.log(`Success: ${stdout}`);
+    });
+  });
